d06b405257328987b573207fb6b293fe5b3c76fb
박진영 <am030813@naver.com>
2024-07-05 19:36:34 +0900

    ADD: 서비스 레이어에 엔드포인트 전달 방식 변경

--- com/booknic/controller/LibraryController.java
+++ com/booknic/controller/LibraryController.java
@@ -2,7 +2,6 @@ package com.booknic.controller;
 
 import static com.booknic.api.LibApi.EndPoint.*;
 
-import com.booknic.api.LibApi;
 import com.booknic.service.LibraryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,106 +20,97 @@ public class LibraryController {
     @GetMapping("/lib")
     public ResponseEntity<?> getLibrary(@RequestParam Map<String, String> params) throws URISyntaxException {
 
-        String endPoint = "/libSrch";
-        List<?> libInfo = LibraryService.getLibInfo(params, endPoint);
+        List<?> libInfo = LibraryService.getLibInfo(params, LIBSRCH.getEndPoint());
         return ResponseEntity.ok().body(libInfo);
     }
     @GetMapping("/item")
     public ResponseEntity<?> getItem(@RequestParam Map<String, String> params) throws URISyntaxException {
 
-        LibApi.EndPoint endPoint = LibApi.EndPoint.valueOf(ITEMSRCH.getEndPoint());
-        List<?> libInfo = LibraryService.getLibInfo(params, String.valueOf(endPoint));
+        List<?> libInfo = LibraryService.getLibInfo(params, ITEMSRCH.getEndPoint());
         return ResponseEntity.ok().body(libInfo);
     }
     @GetMapping("/loanitem")
     public ResponseEntity<?> getLoanItem(@RequestParam Map<String, String> params) throws URISyntaxException {
 
-        LibApi.EndPoint endPoint = LibApi.EndPoint.valueOf(LOANITEMSRCH.getEndPoint());
-        List<?> libInfo = LibraryService.getLibInfo(params, String.valueOf(endPoint));
+        List<?> libInfo = LibraryService.getLibInfo(params, LOANITEMSRCH.getEndPoint());
         return ResponseEntity.ok().body(libInfo);
     }
     @GetMapping("/recommend")
     public ResponseEntity<?> getRecommend(@RequestParam Map<String, String> params) throws URISyntaxException {
 
-        LibApi.EndPoint endPoint = LibApi.EndPoint.valueOf(RECOMMANDLIST.getEndPoint());
-        List<?> libInfo = LibraryService.getLibInfo(params, String.valueOf(endPoint));
+        List<?> libInfo = LibraryService.getLibInfo(params, RECOMMANDLIST.getEndPoint());
         return ResponseEntity.ok().body(libInfo);
     }
     @GetMapping("/detail")
     public ResponseEntity<?> getDetail(@RequestParam Map<String, String> params) throws URISyntaxException {
 
-        LibApi.EndPoint endPoint = LibApi.EndPoint.valueOf(SRCHDTLLIST.getEndPoint());
-        List<?> libInfo = LibraryService.getLibInfo(params, String.valueOf(endPoint));
+        List<?> libInfo = LibraryService.getLibInfo(params, SRCHDTLLIST.getEndPoint());
         return ResponseEntity.ok().body(libInfo);
     }
     @GetMapping("/analysis")
     public ResponseEntity<?> getAnalysis(@RequestParam Map<String, String> params) throws URISyntaxException {
 
-        LibApi.EndPoint endPoint = LibApi.EndPoint.valueOf(USAGEANALYSISLIST.getEndPoint());
-        List<?> libInfo = LibraryService.getLibInfo(params, String.valueOf(endPoint));
+        List<?> libInfo = LibraryService.getLibInfo(params, USAGEANALYSISLIST.getEndPoint());
         return ResponseEntity.ok().body(libInfo);
     }
-    @GetMapping("/loamitemlib")
+    @GetMapping("/loanitemlib")
     public ResponseEntity<?> getLoanItemByLib(@RequestParam Map<String, String> params) throws URISyntaxException {
 
-        LibApi.EndPoint endPoint = LibApi.EndPoint.valueOf(LOANITEMSRCHBYLIB.getEndPoint());
-        List<?> libInfo = LibraryService.getLibInfo(params, String.valueOf(endPoint));
+        List<?> libInfo = LibraryService.getLibInfo(params, LOANITEMSRCHBYLIB.getEndPoint());
         return ResponseEntity.ok().body(libInfo);
     }
     @GetMapping("/trend")
     public ResponseEntity<?> getTrend(@RequestParam Map<String, String> params) throws URISyntaxException {
 
-        LibApi.EndPoint endPoint = LibApi.EndPoint.valueOf(USAGETREND.getEndPoint());
-        List<?> libInfo = LibraryService.getLibInfo(params, String.valueOf(endPoint));
+        List<?> libInfo = LibraryService.getLibInfo(params, USAGETREND.getEndPoint());
         return ResponseEntity.ok().body(libInfo);
     }
     @GetMapping("/exist")
     public ResponseEntity<?> getExist(@RequestParam Map<String, String> params) throws URISyntaxException {
 
-        LibApi.EndPoint endPoint = LibApi.EndPoint.valueOf(BOOKEXIST.getEndPoint());
-        List<?> libInfo = LibraryService.getLibInfo(params, String.valueOf(endPoint));
+        List<?> libInfo = LibraryService.getLibInfo(params, BOOKEXIST.getEndPoint());
         return ResponseEntity.ok().body(libInfo);
     }
     @GetMapping("/hot")
     public ResponseEntity<?> getHotTrend(@RequestParam Map<String, String> params) throws URISyntaxException {
 
-        LibApi.EndPoint endPoint = LibApi.EndPoint.valueOf(HOTTREND.getEndPoint());
-        List<?> libInfo = LibraryService.getLibInfo(params, String.valueOf(endPoint));
+        List<?> libInfo = LibraryService.getLibInfo(params, HOTTREND.getEndPoint());
         return ResponseEntity.ok().body(libInfo);
     }
     @GetMapping("/libsrchbook")
     public ResponseEntity<?> getLibSrchByBook(@RequestParam Map<String, String> params) throws URISyntaxException {
 
-        LibApi.EndPoint endPoint = LibApi.EndPoint.valueOf(LIBSRCHBYBOOK.getEndPoint());
-        List<?> libInfo = LibraryService.getLibInfo(params, String.valueOf(endPoint));
+        List<?> libInfo = LibraryService.getLibInfo(params, LIBSRCHBYBOOK.getEndPoint());
         return ResponseEntity.ok().body(libInfo);
     }
     @GetMapping("/extends/libsrch")
     public ResponseEntity<?> getExLibSrch(@RequestParam Map<String, String> params) throws URISyntaxException {
 
-        LibApi.EndPoint endPoint = LibApi.EndPoint.valueOf(EXTENDLIBSRCH.getEndPoint());
-        List<?> libInfo = LibraryService.getLibInfo(params, String.valueOf(endPoint));
+        List<?> libInfo = LibraryService.getLibInfo(params, EXTENDLIBSRCH.getEndPoint());
         return ResponseEntity.ok().body(libInfo);
     }
     @GetMapping("/extends/loanitemlib")
     public ResponseEntity<?> getExLoanItemByLib(@RequestParam Map<String, String> params) throws URISyntaxException {
 
-        LibApi.EndPoint endPoint = LibApi.EndPoint.valueOf(EXTENDLOANITEMSRCHBYLIB.getEndPoint());
-        List<?> libInfo = LibraryService.getLibInfo(params, String.valueOf(endPoint));
+        List<?> libInfo = LibraryService.getLibInfo(params, EXTENDLOANITEMSRCHBYLIB.getEndPoint());
         return ResponseEntity.ok().body(libInfo);
     }
     @GetMapping("/srchbooks")
     public ResponseEntity<?> getSrchBooks(@RequestParam Map<String, String> params) throws URISyntaxException {
 
-        LibApi.EndPoint endPoint = LibApi.EndPoint.valueOf(SRCHBOOKS.getEndPoint());
-        List<?> libInfo = LibraryService.getLibInfo(params, String.valueOf(endPoint));
+        List<?> libInfo = LibraryService.getLibInfo(params, SRCHBOOKS.getEndPoint());
         return ResponseEntity.ok().body(libInfo);
     }
     @GetMapping("/readQt")
     public ResponseEntity<?> getReadQt (@RequestParam Map<String, String> params) throws URISyntaxException {
 
-        LibApi.EndPoint endPoint = LibApi.EndPoint.valueOf(READQT.getEndPoint());
-        List<?> libInfo = LibraryService.getLibInfo(params, String.valueOf(endPoint));
+        List<?> libInfo = LibraryService.getLibInfo(params, READQT.getEndPoint());
+        return ResponseEntity.ok().body(libInfo);
+    }
+    @GetMapping("/keyword")
+    public ResponseEntity<?> getKeyword(@RequestParam Map<String, String> params) throws URISyntaxException {
+
+        List<?> libInfo = LibraryService.getLibInfo(params, MONTHLYKEYWORDS.getEndPoint());
         return ResponseEntity.ok().body(libInfo);
     }
 }
