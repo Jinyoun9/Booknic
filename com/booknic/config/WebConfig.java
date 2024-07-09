c751f43d134646bd1c7b9e674fd5fb9c26819339
박진영 <am030813@naver.com>
2024-07-09 17:17:39 +0900

    ADD: 인터셉터 우선 순위 결정

---
+++ com/booknic/config/WebConfig.java
@@ -0,0 +1,37 @@
+package com.booknic.config;
+
+import com.booknic.interceptor.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final AuthCheckInterceptor authCheckInterceptor;
+    private final AuthInfoHoldInterceptor authInfoHoldInterceptor;
+    private final LoginSuccessInterceptor loginSuccessInterceptor;
+    private final LogoutSuccessInterceptor logoutSuccessInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInfoHoldInterceptor)
+                .order(1)
+                .addPathPatterns("/api/**");
+
+        registry.addInterceptor(authCheckInterceptor)
+                .order(2)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/login", "/api/signUp", "/api/jwts");
+
+        registry.addInterceptor(loginSuccessInterceptor)
+                .order(3)
+                .addPathPatterns("/api/login");
+
+        registry.addInterceptor(logoutSuccessInterceptor)
+                .order(4)
+                .addPathPatterns("/api/logout");
+    }
+}
