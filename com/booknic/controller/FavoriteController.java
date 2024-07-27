a4a06e329ea0d49b5191eb0b83ba0ba96ff1433c
박진영 <am030813@naver.com>
2024-07-27 23:33:39 +0900

    FEAT: 사용자의 즐겨찾기 도서 목록 불러오기

--- com/booknic/controller/FavoriteController.java
+++ com/booknic/controller/FavoriteController.java
@@ -1,7 +1,11 @@
 package com.booknic.controller;
 
+import com.booknic.dto.BookDto;
+import com.booknic.entity.Favoritebook;
 import com.booknic.entity.User;
+import com.booknic.repository.FavoriteRepository;
 import com.booknic.repository.UserRepository;
+import com.booknic.service.AuthService;
 import com.booknic.service.FavoriteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +14,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.booknic.jwt.*;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "https://localhost:3000")
@@ -18,26 +24,38 @@ import java.util.Map;
 @RequestMapping("/fav")
 public class FavoriteController {
     @Autowired
-    UserRepository userRepository;
-    @Autowired
     FavoriteService favoriteService;
+    @Autowired
+    AuthService authService;
     @PostMapping("/register")
     public ResponseEntity<?> toggleFavorite(@RequestHeader("Authorization") String token, @RequestBody Map<String, String> params) {
-        System.out.println(token);
         try {
             String bookname = params.get("bookname");
             String library = params.get("library");
             String userId = JwtProvider.getId(token, true);
-            User user = userRepository.findUserById(userId);
-            if (user != null) {
-                    favoriteService.addFavoriteBook(bookname, library, user);
+            User user = authService.getUser(userId);
+            if(user != null){
+                favoriteService.addFavoriteBook(bookname, library, user);
                 return ResponseEntity.ok("즐겨찾기 업데이트 성공");
-            } else {
+            }
+            else {
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("사용자에게 접근 권한이 없습니다");
             }
         }catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류 발생");
+        }
+    }
+    @GetMapping("/load")
+    public ResponseEntity<?> getFavoriteBook(@RequestHeader("Authorization") String token){
+        try{
+            String id = JwtProvider.getId(token, true);
+            User user = authService.getUser(id);
+            List<BookDto> favoritebookList = favoriteService.getFavoriteBooks(user);
+            return ResponseEntity.ok(favoritebookList);
+        }
+        catch (Exception e){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("사용자 접근 권한이 없습니다.");
         }
     }
 
