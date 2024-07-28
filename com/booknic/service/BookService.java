576102bbe98290e0dc1dc88ebf7229042b5e3623
박진영 <am030813@naver.com>
2024-07-29 06:22:59 +0900

    REFACTOR: duedate 자료형 LocalDate로 변경

--- com/booknic/service/BookService.java
+++ com/booknic/service/BookService.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -31,17 +32,17 @@ public class BookService {
     public BookService(@Lazy BookEventWebSocketHandler webSocketHandler) {
         this.webSocketHandler = webSocketHandler;
     }
-    LocalDateTime localDateTime = LocalDateTime.now();
-    DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    LocalDate localDate = LocalDate.now();
+
     public void returnBook(String bookname, String library) {
         List<User> userList = favoriteRepository.findDistinctUsersByBooknameAndLibrary(bookname, library);
         System.out.println(userList.size());
         webSocketHandler.sendNotification(bookname, userList);
     }
+
     public void loanBook(String bookname, String library, String token) {
         User user = userRepository.findUserById(JwtProvider.getId(token, true));
-        LocalDateTime dueDateTime = localDateTime.plusWeeks(2);
-        String dueDate = dueDateTime.format(dateTimeFormatter);
+        LocalDate dueDate = localDate.plusWeeks(2);
         Loan newLoan = Loan.builder()
                 .bookname(bookname)
                 .library(library)
@@ -51,4 +52,5 @@ public class BookService {
                 .build();
         loanRepository.save(newLoan);
     }
+
 }
