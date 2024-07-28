f2b795a18a45a510671488879955a98fd8774b5e
박진영 <am030813@naver.com>
2024-07-29 06:21:46 +0900

    FEAT: 사용자의 연체 정보와 대출 정보 불러오기

--- com/booknic/repository/LoanRepository.java
+++ com/booknic/repository/LoanRepository.java
@@ -1,7 +1,14 @@
 package com.booknic.repository;
 
 import com.booknic.entity.Loan;
+import com.booknic.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface LoanRepository extends JpaRepository<Loan, String> {
+
+    List<Loan> findLoansByDuedateBeforeAndLibrary(LocalDate duedate, String library);
+    Loan findLoanByUserAndLibraryAndBookname(User user, String library, String bookname);
 }
